e9a2baeb150dfe8fed0fb102c25cf2a2e1050f04
Furkan <fkaragoz14@gmail.com>
2022-09-26 22:55:51 +0300

    Complete UART echo program.

--- F410_registers_uart_int/Core/Src/main.c
+++ F410_registers_uart_int/Core/Src/main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+#define BUFFER_SIZE		100
+
+uint8_t rxBuffer[ BUFFER_SIZE ] = { 0 };
+uint8_t idx = 0;
+
 int main ( void )
 {
 	system_clock_config();
@@ -13,8 +18,35 @@ int main ( void )
 
 void USART2_IRQHandler ( void )
 {
-	if ( USART2->SR & USART_SR_RXNE )
+	if ( USART2->SR & USART_SR_RXNE )			// Read from DR clears the flag
 	{
-		uart2_send_char( USART2->DR );			// Read from DR clears the flag
+		if ( idx < BUFFER_SIZE )				// Preventing overflow of the buffer
+		{
+			rxBuffer[ idx ] = USART2->DR;
+
+			if ( rxBuffer[ idx ] == '\n' )	// If LF gathered send the received data
+			{
+				uart2_send_data ( rxBuffer );
+
+				idx = 0;
+
+				// Clear the rx buffer. Might be bad method to run inside the int handler though.
+				for ( int j = 0; rxBuffer[ j ] == 0 || j < BUFFER_SIZE ; j++)
+					rxBuffer[ j ] = 0;
+
+				return;
+			}
+
+			idx++;
+		}
+		else
+		{
+			idx = 0;
+		}
+
+	}
+	else
+	{
+		while ( 1 );							// Unexpected, wait for debug
 	}
 }
