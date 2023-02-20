74006d38c19acf82e38757fc2c1251a28ac4b493
Furkan Karagoz <fkaragoz14@gmail.com>
2023-02-21 00:22:47 +0300

    Triangle wave representation by PWM signal.

--- F410_registers_tim_pwm/Core/Src/main.c
+++ F410_registers_tim_pwm/Core/Src/main.c
@@ -8,10 +8,16 @@
 
 #include "main.h"
 
+#define DUTY_STEP		200
+
+static uint8_t rampingUp = 1;
+
 int main ( void )
 {
 	system_clock_config ();
-	tim11_config ( 1000, 50000 );
+	tim11_config ( 1000, 5000 );
+
+	TIM11->CCR1 = 0;
 
 	tim11_enable ( 1 );
 
@@ -19,4 +25,42 @@ int main ( void )
 		;
 
 	return 0;
+}
+
+
+void TIM1_TRG_COM_TIM11_IRQHandler ( void )
+{
+	if ( TIM11->SR & TIM_SR_UIF )
+	{
+		if ( rampingUp )	// Ramping up
+		{
+			if ( TIM11->CCR1 <= TIM11->ARR )
+			{
+				TIM11->CCR1 += DUTY_STEP;
+			}
+			else					// Reached top, go down
+			{
+				rampingUp = 0;
+				TIM11->CCR1 -= DUTY_STEP;
+			}
+		}
+		else	// Ramping down
+		{
+			if ( TIM11->CCR1 > 0 )
+			{
+				TIM11->CCR1 -= DUTY_STEP;
+			}
+			else					// Reached bottom, go up
+			{
+				rampingUp = 1;
+				TIM11->CCR1 += DUTY_STEP;
+			}
+		}
+
+		TIM11->SR &= ~TIM_SR_UIF;	// Clear update interrupt flag
+	}
+//	else if ( TIM11->SR & TIM_SR_CC1IF )
+//	{
+//		TIM11->SR &= ~TIM_SR_CC1IF;	// Clear CC interrupt flag
+//	}
 }
--- F410_registers_tim_pwm/Core/Src/tim.c
+++ F410_registers_tim_pwm/Core/Src/tim.c
@@ -104,7 +104,7 @@ void tim11_config ( uint16_t psc, uint16_t arr )
 	//! 4. Set the timing parameters
 	TIM11->PSC = psc - 1;										// Set prescaler
 	TIM11->ARR = arr;											// Set auto-reload
-	TIM11->CCR1 = arr / 3;										// 50% duty cycle
+	TIM11->CCR1 = arr / 2;										// 50% duty cycle
 
 	//! 5. Set PWM mode and enable output compare preload register
 	TIM11->CCMR1 |= ( 0x6UL << TIM_CCMR1_OC1M_Pos );			// PWM mode 1
